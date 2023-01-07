f72de7b2129962eb1363803365e7403b482f5b2a
mstoecklein <mstoecklein@xp.live>
2023-01-07 01:23:00 +0100

    reorder

--- dest/idbx.js
+++ dest/idbx.js
@@ -34,10 +34,10 @@ function add(store, item, key) {
     return asyncRequest(store.add(item, key));
 }
 export { add as add };
-function bulkAdd(store, items = [], key) {
+function addBulk(store, items = [], key) {
     return Promise.all(items.map((item)=>add(store, item, key)));
 }
-export { bulkAdd as bulkAdd };
+export { addBulk as addBulk };
 function put(store, item, key) {
     return asyncRequest(store.put(item, key));
 }
@@ -190,73 +190,3 @@ function batch(db, commands, mode) {
     };
 }
 export { batch as batch };
-function asyncIterator(req, onSuccess, onError) {
-    let next = null;
-    let error = null;
-    let promise;
-    const nextPromise = ()=>new Promise((resolve, reject)=>{
-            next = resolve;
-            error = reject;
-        });
-    promise = nextPromise();
-    req.addEventListener("success", (ev)=>{
-        const target = ev.target;
-        if (onSuccess === undefined) {
-            next?.({
-                value: target.result,
-                done: false
-            });
-        } else {
-            onSuccess(target.result, (result)=>{
-                next?.(result);
-                promise = nextPromise();
-            });
-        }
-    });
-    req.addEventListener("error", (ev)=>{
-        const target = ev.target;
-        if (onError === undefined) {
-            error?.(new Error(`IndexedDB error: ${target.error?.message}`));
-        } else {
-            onError(target, error);
-        }
-    });
-    return {
-        next () {
-            return promise;
-        },
-        return () {
-            return Promise.resolve({
-                value: undefined,
-                done: true
-            });
-        },
-        throw (error) {
-            return Promise.reject(error);
-        },
-        [Symbol.asyncIterator] () {
-            return this;
-        }
-    };
-}
-function openCursorIterator(store, query, direction) {
-    return asyncIterator(store.openCursor(query, direction));
-}
-export { openCursorIterator as openCursorIterator };
-function openKeyCursor(store, query, direction) {
-    return asyncIterator(store.openKeyCursor(query, direction), (value, next)=>{
-        if (value) {
-            next({
-                value,
-                done: false
-            });
-            value.continue();
-        } else {
-            next({
-                value: null,
-                done: true
-            });
-        }
-    });
-}
-export { openKeyCursor as openKeyCursor };
